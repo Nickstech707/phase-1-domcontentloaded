64542c5fb19f3cfe0cd0d13838f603c8b4361c0d
Nicholas Muriithi <nickstech707@gmail.com>
2024-04-02 09:07:49 +0300

    Set up an event on DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// Wait for the DOM to be loaded and then call the updateDOM function when the DOM is ready
+document.addEventListener("DOMContentLoaded", function () {
+  updateDOM();
+});
+
+// Function to update the content of an HTML element with id "text" and set it to "This is really cool!"
+function updateDOM() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
